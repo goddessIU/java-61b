03db3cab563c06f789a3f02896d2af426d2cdbbd
goddessIU <2581606394@qq.com>
2023-11-30 15:41:11 +0800

    lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,13 @@
  */
 public class LeapYear {
 
+    private static boolean isLeapYear(int year) {
+        if ((year % 4== 0 && year % 100 != 0) || year % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
