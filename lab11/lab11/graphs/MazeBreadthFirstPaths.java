ce598554af841fc4130eac1f5c8efd95276c7720
goddessIU <2581606394@qq.com>
2023-12-06 16:09:30 +0800

    lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -10,20 +14,58 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    private void bfs(int v) {
+        marked[v] = true;
+        announce();
+
+        if (v == t) {
+            targetFound = true;
+        }
+
+        if (targetFound) {
+            return;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+        while (!queue.isEmpty()) {
+            int node = queue.remove();
+            for (int w : maze.adj(node)) {
+                if (!marked[w]) {
+                    edgeTo[w] = node;
+                    marked[w] = true;
+                    announce();
+                    distTo[w] = distTo[node] + 1;
+                    queue.add(w);
+                    if (w == t) {
+                        targetFound = true;
+                        return;
+                    }
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
         // bfs();
+        bfs(s);
     }
 }
 
